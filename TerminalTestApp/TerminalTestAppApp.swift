fba36de977f57afc0f391d460ebbef0a53a14ecf
HajarNashi <HajarNashi@gmail.com>
2022-11-11 21:19:48 +0300

    Initial Commit

---
+++ TerminalTestApp/TerminalTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TerminalTestAppApp.swift
+//  TerminalTestApp
+//
+//  Created by Hajar Nashi on 17/04/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct TerminalTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
